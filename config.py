040644fbb87c2e20de7dd8574624dd4c26d1b611
loukayl <79642918+loukayl@users.noreply.github.com>
2021-03-16 16:21:19 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Can be multiple prefixes, like this: ("!", "?")
+BOT_PREFIX = ("v")
+TOKEN = "NzgyNDE5MTI4NTk3NjEwNTE3.X8L6mA.L9zCEQE82oLJ2jOrrxYVXxgU-fA"
+APPLICATION_ID = "782419128597610517"
+OWNERS = [674700670929993769, 460129131783389184]
+BLACKLIST = []
+ # Default cogs that I have created for the template
+STARTUP_COGS = [
+    "cogs.general", "cogs.userlevels", "cogs.music", "cogs.moderation", "cogs.owner", "cogs.reactions", "cogs.Twitch", "cogs.TwitchApiPy", "cogs.tc",
+]
